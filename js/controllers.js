fc5d644aa9fee98145d3de3683b5082a21a7aa47
xandros-na <xandros-na>
2014-11-28 19:46:54 -0500

    can get token from server

--- js/controllers.js
+++ js/controllers.js
@@ -1,24 +1,34 @@
 var trackerAppControllers = angular.module('trackerAppControllers', []);
 
 
-trackerAppControllers.controller("SignInFormCtrl",["$scope", function($scope){
-	$scope.title="Sign In";
-	$scope.submitButton="Sign In";
-	$scope.switch="I don't have an account yet.";
+trackerAppControllers.controller("SignInFormCtrl", ["$scope", function ($scope) {
+    $scope.title = "Sign In";
+    $scope.submitButton = "Sign In";
+    $scope.switch = "I don't have an account yet.";
 
-	$scope.submitForm = function(){
-		alert("asdf");
-	};
-	$scope.contentSwitch=function(){
-	if(!($scope.switch==="Sign In")){
-		$scope.title="Sign Up";
-		$scope.submitButton="Register";
-		$scope.switch="Sign In";
-	}
-	else{
-		$scope.title="Sign In";
-		$scope.submitButton="Sign In";
-		$scope.switch="I don't have an account yet.";
-	}
-	};
+    $scope.submitForm = function () {
+        var creds = JSON.stringify({'username': $scope.username, 'password': $scope.password});
+        $.ajax({
+            url: 'http://cp317api.pythonanywhere.com/api/register',
+            data: creds,
+            type: 'POST',
+            contentType: 'application/json',
+            dataType: 'json',
+            success: function (data) {
+                localStorage['token'] = JSON.stringify(data);
+            }
+        });
+    };
+    $scope.contentSwitch = function () {
+        if (!($scope.switch === "Sign In")) {
+            $scope.title = "Sign Up";
+            $scope.submitButton = "Register";
+            $scope.switch = "Sign In";
+        }
+        else {
+            $scope.title = "Sign In";
+            $scope.submitButton = "Sign In";
+            $scope.switch = "I don't have an account yet.";
+        }
+    };
 }]);
