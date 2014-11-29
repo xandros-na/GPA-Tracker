b1ae6f21cf103fd24bfb7035b626d698541e2051
xandros-na <xandros-na>
2014-11-29 17:35:09 -0500

    added ian's changes

--- js/controllers.js
+++ js/controllers.js
@@ -1,128 +1,51 @@
 var trackerAppControllers = angular.module('trackerAppControllers', []);
 
 
-trackerAppControllers.controller("SignInFormCtrl", ["$scope", function ($scope) {
-    $scope.title = "Sign In";
-    $scope.submitButton = "Sign In";
-    $scope.switch = "I don't have an account yet.";
+trackerAppControllers.controller("SignInFormCtrl",["$scope", function($scope){
+	$scope.title="Sign In";
+	$scope.submitButton="Sign In";
+	$scope.switch="I don't have an account yet.";
 
-    $scope.submitForm = function () {
-        var creds = JSON.stringify({'username': $scope.username, 'password': $scope.password});
-//        $.ajax({
-//            url: 'http://cp317api.pythonanywhere.com/api/register',
-//            data: creds,
-//            type: 'POST',
-//            contentType: 'application/json',
-//            dataType: 'json',
-//            success: function (data) {
-//                localStorage['token'] = JSON.stringify(data);
-//            }
-//        });
-        $.ajax({
-            url: 'http://cp317api.pythonanywhere.com/api/login',
-            type: 'POST',
-            dataType: 'json',
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader("Authorization", "Basic" + " " + btoa($scope.username + ":" + $scope.password));
-            },
-            success: function (data) {
-                console.log(data);
-            }
-        });
-    };
-    $scope.contentSwitch = function () {
-        if (!($scope.switch === "Sign In")) {
-            $scope.title = "Sign Up";
-            $scope.submitButton = "Register";
-            $scope.switch = "Sign In";
-        }
-        else {
-            $scope.title = "Sign In";
-            $scope.submitButton = "Sign In";
-            $scope.switch = "I don't have an account yet.";
-        }
-    };
+	$scope.submitForm = function(user){
+		if(!($scope.switch==="Sign In")){
+			//check with server and download profile
+		}
+		else{
+			//check with server and create profile
+		}
+
+	};
+	
+	$scope.contentSwitch=function(){
+		if(!($scope.switch==="Sign In")){
+			$scope.title="Sign Up";
+			$scope.submitButton="Register";
+			$scope.switch="Sign In";
+		}
+		else{
+			$scope.title="Sign In";
+			$scope.submitButton="Sign In";
+			$scope.switch="I don't have an account yet.";
+		}
+	};
 }]);
 
-trackerAppControllers.controller("TermCtrl", ["$scope", function ($scope) {
-    $scope.terms = [
-        {'name': 'd2014'},
-        {'name': 'b2003'},
-        {'name': 'a2003'},
-        {'name': 'c2003'}
-    ];
-
+trackerAppControllers.controller("TermCtrl",["$scope", function($scope){
+	//get term list from DB
+	$scope.terms=[{'name': 'd2014'}, {'name': 'b2003'},{'name': 'a2003'},{'name': 'c2003'}];
 }]);
 
-trackerAppControllers.controller("CourseCtrl", ["$scope", function ($scope) {
-    $scope.courses = [
-        {'name': 'ma205'},
-        {'name': 'cp114'},
-        {'name': 'cp217'},
-        {'name': 'ma218'}
-    ];
-
+trackerAppControllers.controller("CourseCtrl",["$scope", "$routeParams", function($scope, $routeParams){
+	//get termName
+	console.log($routeParams.termName);
+	//get course list from DB
+	$scope.courses=[{'name': 'ma205'}, {'name': 'cp114'},{'name': 'cp217'},{'name': 'ma218'}];
+	
 }]);
 
-trackerAppControllers.controller("AssessmentsCtrl", ["$scope", function ($scope) {
-    var data = {'2014': {'cp': {'goal': 80, 'distance': 80, 'details': {'midterm': null, 'quizzes': null}}}};
-    localStorage['gpa_user'] = JSON.stringify(data);
-    //$scope.assessments = [ {'name': 'zzz'}, {'name': 'aaa'} ]
-    var as = get_assessments('2014', 'cp');
-    var a = [];
-    for (var i in as) {
-        var b = {};
-        b['name'] = as[i]; // ->> {'name': as[i]}
-        a.push(b); // ->> [ {'name': as[i]} ]
-    }
-    $scope.assessments = a; //-->> [ {'name': 'zzz'}, {'name': 'aaa'} ]
-    $scope.orderProp = 'name';
-
+trackerAppControllers.controller("AssessmentCtrl",["$scope", "$routeParams", function($scope, $routeParams){
+	//get courseName
+	console.log($routeParams.courseName);
+	//get course list from DB
+	$scope.assessment=[];
 }]);
-
-trackerAppControllers.controller("MarksCtrl", ["$scope", function ($scope) {
-    var data = {'2014': {'cp': {'goal': 80, 'distance': 80, 'details': {'midterm': {'list': {'m1': 80, 'm2': 80}}, 'quizzes': {'list': {'q1': 80, 'q2': 70}}}}}};
-    localStorage['gpa_user'] = JSON.stringify(data);
-    //$scope.assessments = [ {'name': 'zzz'}, {'name': 'aaa'} ]
-    var as = get_marks('2014', 'cp', 'quizzes');
-    var keys = Object.keys(as);
-    console.log(keys.length == as.length);
-    var a = [];
-    for (var i = 0; i < keys.length; i++) {
-        var b = {};
-        console.log(keys[i], as[keys[i]]);
-        b['name'] = keys[i];
-        b['mark'] = as[keys[i]]; // ->> {'name': as[i]}
-        a.push(b); // ->> [ {'name': as[i]} ]
-    }
-    $scope.marks = a; //-->> [ {'name': 'zzz'}, {'name': 'aaa'} ]
-    $scope.orderProp = 'name';
-
-}]);
-
-trackerAppControllers.controller('AccordionDemoCtrl', function ($scope) {
-  $scope.oneAtATime = true;
-
-  $scope.groups = [
-    {
-      title: 'Dynamic Group Header - 1',
-      content: 'Dynamic Group Body - 1'
-    },
-    {
-      title: 'Dynamic Group Header - 2',
-      content: 'Dynamic Group Body - 2'
-    }
-  ];
-
-  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
-
-  $scope.addItem = function() {
-    var newItemNo = $scope.items.length + 1;
-    $scope.items.push('Item ' + newItemNo);
-  };
-
-  $scope.status = {
-    isFirstOpen: true,
-    isFirstDisabled: false
-  };
-});
--- js/tracker.js
+++ js/tracker.js
@@ -1,19 +1,29 @@
+
 var trackerApp = angular.module('tracker', [
-    'ui.bootstrap',
-    'ngRoute',
-    'trackerAppControllers'
+  'ngRoute',
+  'trackerAppControllers'
 ]);
 
 trackerApp.config(['$routeProvider',
-    function ($routeProvider) {
-        $routeProvider.
-            when('/assessments', {
-                templateUrl: 'html/assessment.html',
-		controller: 'AccordionDemoCtrl'
-//                controller: 'CourseCtrl'
-//        controller: 'SignInFormCtrl'
-            }).
-            otherwise({
-                redirectTo: '/assessments'
-            });
-    }]);
+  function($routeProvider) {
+    $routeProvider.
+      when('/login', {
+        templateUrl: 'html/login.html',
+        controller: 'SignInFormCtrl'
+      }).
+	  when('/term', {
+        templateUrl: 'html/term.html',
+		controller: 'TermCtrl'
+      }).
+	  when('/courseList/:termName', {
+        templateUrl: 'html/course.html',
+		controller: 'CourseCtrl'
+      }).
+	  when('/course/:courseName', {
+		templateUrl: 'html/courseDetail.html',
+    	controller: 'AssessmentCtrl'
+      }).
+      otherwise({
+        redirectTo: '/login'
+      });
+  }]);
