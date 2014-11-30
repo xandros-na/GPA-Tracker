e25660066f25e63f2a7158970b4dc75290f516bc
xandros-na <xandros-na>
2014-11-29 20:16:09 -0500

    merged perlanie's changes

--- js/controllers.js
+++ js/controllers.js
@@ -72,3 +72,82 @@ trackerAppControllers.controller("CourseCtrl", ["$scope", "$routeParams", functi
         delete_course(term, course);
     };
 }]);
+
+trackerAppControllers.controller("AssessmentsCtrl", ["$scope", "$modal", function ($scope, $modal) {
+    var data = {'2014': {'cp': {'goal': 80, 'distance': 80, 'details': {'midterm': {'list': {'m1': 80, 'm2': 80}}, 'quizzes': {'list': {'q1': 80, 'q2': 70}}}}}};
+    localStorage['gpa_user'] = JSON.stringify(data);
+    //$scope.assessments = [ {'name': 'zzz'}, {'name': 'aaa'} ]
+    var getAssess = get_assessments('2014', 'cp');
+    var assessment = [];
+    for (var i in getAssess) {
+        var a = {};
+        a['name'] = getAssess[i]; // ->> {'name': as[i]}
+        assessment.push(a); // ->> [ {'name': as[i]} ]
+    }
+    $scope.assessments = assessment; //-->> [ {'name': 'miderm'}, {'name': 'quizzes'} ]
+    $scope.orderProp = 'name';
+
+
+
+	var getMarks = get_marks('2014', 'cp', 'quizzes');
+    var keys = Object.keys(getMarks);
+    console.log(keys.length == getMarks.length);
+    var mark = [];
+    for (var j = 0; j< keys.length; j++) {
+        var b = {};
+        console.log(keys[j], getMarks[keys[j]]);
+        b['name'] = keys[j]; //->> {'name': q1}
+        b['mark'] = getMarks[keys[j]]; // ->> {'mark': as[i]}
+        mark.push(b); // ->> [ {object} ]
+    }
+    $scope.items = mark; //-->> [ {'name': 'q1', 'mark': 11}]
+    $scope.orderProp = 'name';
+	  //$scope.items = ['item1', 'item2', 'item3'];
+  $scope.open = function (size) {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+}]);
+
+trackerAppControllers.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+	$modalInstance.close($scope.selected.item);
+    var term = "2014";
+	var course = "cp";
+	var as = "quizzes";
+	var mark = $scope.assessmentText;
+	console.log(mark);
+
+	add_mark(term,course,as,"q4",mark);
+	var b = {};
+	b['name'] = "q44"; //->> {'name': q1}
+    b['mark'] = mark;
+	$scope.items.push(b);
+
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+});
--- js/tracker.js
+++ js/tracker.js
@@ -1,29 +1,34 @@
-
 var trackerApp = angular.module('tracker', [
-  'ngRoute',
-  'trackerAppControllers'
+    'ui.bootstrap',
+    'ngRoute',
+    'trackerAppControllers'
 ]);
 
 trackerApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/login', {
-        templateUrl: 'html/login.html',
-        controller: 'SignInFormCtrl'
-      }).
-	  when('/term', {
-        templateUrl: 'html/term.html',
-		controller: 'TermCtrl'
-      }).
-	  when('/courseList/:termName', {
-        templateUrl: 'html/course.html',
-		controller: 'CourseCtrl'
-      }).
-	  when('/course/:courseName', {
-		templateUrl: 'html/courseDetail.html',
-    	controller: 'AssessmentCtrl'
-      }).
-      otherwise({
-        redirectTo: '/login'
-      });
-  }]);
+    function ($routeProvider) {
+        $routeProvider.
+            when('/login', {
+                templateUrl: 'html/login.html',
+                controller: 'SignInFormCtrl'
+            }).
+            when('/term', {
+                templateUrl: 'html/term.html',
+                controller: 'TermCtrl'
+            }).
+            when('/courseList/:termName', {
+                templateUrl: 'html/course.html',
+                controller: 'CourseCtrl'
+            }).
+            when('/course/:courseName', {
+                templateUrl: 'html/courseDetail.html',
+                controller: 'AssessmentCtrl'
+            }).
+            when('/assessments', {
+                templateUrl: 'html/assessment.html',
+                controller: 'AssessmentsCtrl'
+            }).
+            otherwise({
+                redirectTo: '/assessments'
+            });
+
+    }]);
